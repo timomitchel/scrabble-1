48d8208d881f9e679de413c9a029bbffacc12c1c
Tim Tyrrell <timomitchel23@gmail.com>
2017-10-18 10:30:36 -0600

    all tests passing

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,7 +1,54 @@
+require "pry"
+
 class Scrabble
 
   def score(word)
-    1
+    return 0 if word == "" or word == nil
+    score_mapper(word).sum
+  end
+
+  def score_mapper(word)
+    formatted = word.upcase.chars
+    formatted.map do |char|
+      point_values[char]
+    end
+  end
+
+  def bonus(word)
+    bonus = 0
+    bonus = 10 if word.length > 6
+    bonus
+  end
+
+  def score_multiplier(word, score_with_multipliers)
+    score_mapper(word).each_with_index.map do |score, index|
+      score * score_with_multipliers[index]
+    end
+  end
+
+  def score_with_multipliers(word, score_with_multipliers, word_multiplier = 1)
+    current_bonus = bonus(word)
+    new_scores = score_multiplier(word, score_with_multipliers)
+    (new_scores.sum + current_bonus) * word_multiplier
+  end
+
+  def word_mapper(words)
+    words.map do |word|
+      score_mapper(word)
+    end
+  end
+
+  def word_score_summer(scores)
+    scores.map do |score|
+      score.sum
+    end
+  end
+
+  def highest_scoring_word(words)
+    scores = word_mapper(words)
+    summed = word_score_summer(scores)
+    highest_scorer = summed.zip(words).max
+    highest_scorer[1]
   end
 
   def point_values
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -6,8 +6,53 @@ require 'pry'
 
 class ScrabbleTest < Minitest::Test
   def test_it_can_score_a_single_letter
-    skip
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
   end
+
+  def test_can_score_multiple_letters
+    game = Scrabble.new.score("hello")
+    assert_equal 8, game
+  end
+
+  def test_blank_or_nil_entries_result_in_0
+    game_1 = Scrabble.new.score('')
+    game_2 = Scrabble.new.score(nil)
+
+    assert_equal 0, game_1
+    assert_equal 0, game_2
+  end
+
+  def test_can_score_with_multipliers
+    game = Scrabble.new
+
+    assert_equal 9, game.score_with_multipliers('hello', [1,2,1,1,1])
+    assert_equal 18, game.score_with_multipliers('hello', [1,2,1,1,1], 2)
+    assert_equal 58, game.score_with_multipliers('sparkle', [1,2,1,3,1,2,1], 2)
+  end
+
+  def test_find_highest_word_score
+    game = Scrabble.new
+
+    assert_equal 'home', game.highest_scoring_word(['home', 'word', 'hello', 'sound'])
+  end
+
+  def test_highest_word_score_returns_word_with_fewest_letters_if_ties
+    game = Scrabble.new
+
+    assert_equal 'word', game.highest_scoring_word(['hello', 'word', 'sound'])
+  end
+
+  def test_seven_letter_words_best
+    game = Scrabble.new
+
+    assert_equal 'silence', game.highest_scoring_word(['home', 'word', 'silence'])
+  end
+
+  def test_picks_first_word_if_tie_between_length_and_score
+    game = Scrabble.new
+
+    assert_equal "word", game.highest_scoring_word(['hi', 'word', 'ward'])
+  end
+
 end
